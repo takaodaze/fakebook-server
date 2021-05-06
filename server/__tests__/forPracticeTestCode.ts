3b90aeb1dba470e7e74ad609780faae48eb8b1b2
takaodaze <gin2798@gmail.com>
2021-05-07 02:32:02 +0900

    add code for practice

---
+++ server/__tests__/forPracticeTestCode.ts
@@ -0,0 +1,21 @@
+// source code for practice
+
+import { bi } from "../src/forPracticeCode";
+
+describe("practice test", () => {
+  it.skip("test 1.", () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  it("test 2.", () => {
+    const testData = 3;
+    const actual = bi(testData);
+    expect(actual).toBe(testData * 2);
+  });
+
+  it.only("test 3.", () => {
+    const testData = 3;
+    const actual = bi(testData);
+    expect(actual).toBe(testData * 2);
+  });
+});
---
+++ server/src/forPracticeCode.ts
@@ -0,0 +1,6 @@
+import database from "./db";
+// source code for practice
+
+export const bi = (n: number) => {
+  return 2 * n;
+};
