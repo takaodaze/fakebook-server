1cec205f21e338d756ef810e5c4b030c53f041b4
takaodaze <gin2798@gmail.com>
2021-05-08 03:29:51 +0900

    eslint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 // import { connectProdDB } from "DBConnecter";
 import express from "express";
 
-const startServer = async (): Promise<void> => {
+const startServer = (): void => {
   // const db = await connectProdDB();
   const port = 8000;
   const server = express();
