8efdec53cffe066aec1fa5de77ca2c91b6564136
takaodaze <gin2798@gmail.com>
2021-05-07 21:49:56 +0900

    test commit

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,6 @@ import express from "express";
 import { create } from "./db/Users";
 
 const startServer = async () => {
-  const aa = "aaa..s";
   const db = await connectProdDB();
   const port = 8000;
   const server = express();
