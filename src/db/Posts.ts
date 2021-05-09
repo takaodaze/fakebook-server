7ef200c82f0541201e24f26c6e6277a0104bde66
takaodaze <gin2798@gmail.com>
2021-05-09 22:13:00 +0900

    implemented posts module

---
+++ src/db/Posts.ts
@@ -0,0 +1,28 @@
+import { execQuery, ExecResult } from "./utils";
+import * as Logger from "./logger";
+import { Connection } from "mysql";
+
+export const TABLE_NAME = "posts";
+export const initTableSql = `
+    CREATE TABLE ${TABLE_NAME} (
+      post_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+      uid VARCHAR(100) NOT NULL,
+      description VARCHAR(200) NOT NULL
+    )
+`;
+
+export async function createTable(db: Connection): Promise<ExecResult> {
+  return await execQuery(db, initTableSql);
+}
+
+export async function create(
+  db: Connection,
+  uid: string,
+  description: string
+): Promise<ExecResult> {
+  const sql = `INSERT INTO ${TABLE_NAME} SET ?`;
+  const data = { uid, description };
+  const result = await execQuery(db, sql, data);
+  Logger.log("INSERT", TABLE_NAME, data);
+  return result;
+}
